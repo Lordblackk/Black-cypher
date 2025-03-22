2f4ae67f68f4c50f9c9418399ef0b28e5bd8eff3
Lordblackk <olorunfemidavid16@gmail.com>
2025-03-22 10:25:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Dr Black',
   botName: "Black Cypher",
   botOwner: 'Dr Black', // Your name
-  ownerNumber: '09138456125', //Your number
+  ownerNumber: '+2349138456125', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
